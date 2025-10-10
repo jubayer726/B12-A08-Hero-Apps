9222a015b60edd0dd39d0c0eb3971d080211a4ed
JUBAYER <juelali482@gmail.com>
2025-10-10 17:15:29 +0600

    added more funciton

--- src/Pages/AppDetails.jsx
+++ src/Pages/AppDetails.jsx
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import UseApps from '../Hook/UseApps';
 import { useParams } from 'react-router';
 import download from '/icon-downloads.png'
-import ratings from '/icon-ratings.png'
+import Ratings from '/icon-ratings.png'
 import review from '/icon-review.png'
-// import Barchat from '/.Barchat.jsx';
+import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { ToastContainer, toast } from 'react-toastify';
 import logo from '/logo.png'
 
@@ -20,7 +20,7 @@ const AppDetails = () => {
           />
         </div> )
 
-    const {image, title, description, ratingAvg, downloads, reviews, size} = app;
+    const {image, title, description, ratingAvg, downloads, reviews, size, ratings} = app;
 
     const handleInstall =() =>{
         toast("The App installing..")
@@ -58,7 +58,7 @@ const AppDetails = () => {
                            <h1 className="font-bold lg:text-3xl">{downloads}M</h1>
                         </div>
                         <div>
-                           <img src={ratings} alt="photo" />
+                           <img src={Ratings} alt="photo" />
                            <p className='text-gray-500 py-2'>Average Ratings</p>
                            <h1 className=" font-bold lg:text-3xl">{ratingAvg}</h1>
                         </div>
@@ -72,15 +72,37 @@ const AppDetails = () => {
                 </div>
             </div>
             <hr className="border-t border-gray-300 my-4" />
+
+            <div className='space-y-5 my-5'>
+                <h2 className='text-3xl font-bold'>Ratings</h2>
+                <div className='bg-base-100 rounded-2xl p-4 h-80'>
+                    <ResponsiveContainer width="100%" height="100%">
+                        <BarChart
+                        
+                            data={ratings}
+                            margin={{ top: 5, right: 30, left: 50, bottom: 5 }}
+                        >
+                            <CartesianGrid strokeDasharray="3 3" />
+                            <XAxis dataKey="ratings" />
+                            <YAxis />
+                            <Tooltip />
+                            <Legend />
+                             <Bar dataKey="count" fill="#FF8C00" barSize={25} radius={[4, 4, 4, 4]} />
+                        </BarChart>
+                        </ResponsiveContainer>
+
+                </div>
+            </div>
+
+            <hr className="border-t border-gray-300 my-4" />
             <div className='p-5'>
                 <h1 className='p-5 text-2xl font-semibold'>Description</h1>
                 <p className='text-gray-500'>{description}</p>
-                
             </div>
-            {/* <Barchat></Barchat> */}
+
             <ToastContainer
                 position="top-center"
-                autoClose={5000}
+                autoClose={4000}
                 theme="dark"
                 />
         </div>
